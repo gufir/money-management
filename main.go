a9f9f9473aee18ac341b0eb69a55bcb07d498a71
gufir <gunawan.firdaus99@ymail.com>
2025-01-18 17:41:32 +0700

    feat(grpc): add dbdoc and grpc server

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rakyll/statik/fs"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	"github.com/gufir/money-management/api"
 	db "github.com/gufir/money-management/db/sqlc"
+	_ "github.com/gufir/money-management/doc/statik"
 	"github.com/gufir/money-management/gapi"
 	"github.com/gufir/money-management/pb"
 	"github.com/gufir/money-management/utils"
@@ -28,6 +33,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGRPCServer(config, store)
 }
 
@@ -62,6 +68,60 @@ func runGRPCServer(config utils.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Error().Err(err).Msg("cannot start server")
+	}
+
+}
+
+func runGatewayServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal().Msg("cannot create server")
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterMoneyManagementHandlerServer(ctx, grpcMux, server)
+
+	if err != nil {
+		log.Fatal().Msg("cannot register service")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal().Msg("cannot create statik file system")
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+
+	if err != nil {
+		log.Fatal().Msgf("cannot listen to gRPC server on %s: %v", config.GRPCServerAddress, err)
+	}
+
+	log.Info().Msgf("start HTTP gateway server on %s", listener.Addr().String())
+	handler := gapi.HttpLogger(mux)
+
+	err = http.Serve(listener, handler)
+	if err != nil {
+		log.Fatal().Msg("cannot start server")
 	}
 
 }
